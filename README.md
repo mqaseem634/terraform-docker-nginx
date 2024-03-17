3e5fa2692d1af2ca4d16e227e76404c3fdc5464c
Qaseem Zalmai <mqaseem634@gmail.com>
2024-03-17 13:17:52 -0700

    Adding Docker provider and NGINX container

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-docker-nginx
 This terraform module deploys an NGINX container to the Docker Engine
+
+This module is developed by Mohammad.
---
+++ docker_container_nginx.tf
@@ -0,0 +1,9 @@
+# Start a container
+resource "docker_container" "nginx" {
+  name  = "nginx"
+  image = "docker.io/nginx:latest"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
